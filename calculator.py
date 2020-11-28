3ced9d68a092a481b4af19bd9b0b0d978a81c861
BaguDev <74671082+BaguDev@users.noreply.github.com>
2020-11-28 16:20:52 -0500

    Calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,29 @@
- print('Python Calculator | By Crevis')
-def is_float(var):
+ print('Python Calculator | By BaguDev')
+import itertools
+import threading
+import time
+import sys
+
+done = False
+#here is the animation
+def animate():
+    for c in itertools.cycle(['.', '..', '...', '..']):
+        if done:
+            break
+        sys.stdout.write('\rloading ' + c)
+        sys.stdout.flush()
+        time.sleep(0.1)
+    sys.stdout.write('\rDone!')
+    print('Type in a number\n')
+
+t = threading.Thread(target=animate)
+t.start()
+
+#long process here
+time.sleep(10)
+done = True
+
+ def is_float(var):
     try:
         float(var)
         return True
